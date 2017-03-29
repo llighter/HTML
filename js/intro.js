477e60da2a0ac142abc2ffe72f50df8d35468ee2
llighter <hadbsgk@gmail.com>
2017-03-29 15:38:42 +0900

    css/figure.css 추가, js/intro.js 추가

---
+++ js/intro.js
@@ -0,0 +1,22 @@
+var score = 0;
+
+// 나이 값을 표시함
+function myAge() {
+    var age = document.getElementById("age").value;
+    document.getElementById("ageString").innerHTML=age+"살?";
+}
+
+function call() {
+
+    if(form01.age.value == 28) { score += 25; }
+    if(form01.hobby[1].checked && form01.hobby[2].checked) { score += 25; }
+    if(form01.bloodType[0].checked) { score += 25; }
+    if(form01.season.value == "fall") { score += 25; }
+
+    alert(score + "점\n"
+        + "저는 28살 입니다." + "\n"
+        + "저는 책읽기와 게임하기를 좋아합니다." + "\n"
+        + "제 혈액형은 A형이지만 소심하진 않아요." + "\n"
+        + "그리고 전 가을을 좋아합니다." + "\n");
+
+}
